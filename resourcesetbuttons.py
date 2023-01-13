aceee9620c67e248552e5579ba740827d6d4b4e4
Will Mitchell <liquidcitymotors@gmail.com>
2023-01-12 18:45:07 -0600

    Implement led style buttons for sync3 scale editor

--- resourcesetbuttons.py
+++ resourcesetbuttons.py
@@ -1,33 +1,39 @@
 from PySide6.QtWidgets import QPushButton
+from PySide6.QtCore import QSize
 
-class Slot1Button(QPushButton):
+class SlotButton(QPushButton):
+	def __init__(self, *args, **kwargs):
+		super(SlotButton, self).__init__(*args, **kwargs)
+		#self.setFixedSize(QSize(30, 30))
+
+class Slot1Button(SlotButton):
 	def __init__(self, *args, **kwargs):
 		super(Slot1Button, self).__init__(*args, **kwargs)
 
-class Slot2Button(QPushButton):
+class Slot2Button(SlotButton):
 	def __init__(self, *args, **kwargs):
 		super(Slot2Button, self).__init__(*args, **kwargs)
 
-class Slot3Button(QPushButton):
+class Slot3Button(SlotButton):
 	def __init__(self, *args, **kwargs):
 		super(Slot3Button, self).__init__(*args, **kwargs)
 
-class Slot4Button(QPushButton):
+class Slot4Button(SlotButton):
 	def __init__(self, *args, **kwargs):
 		super(Slot4Button, self).__init__(*args, **kwargs)
 
-class Slot5Button(QPushButton):
+class Slot5Button(SlotButton):
 	def __init__(self, *args, **kwargs):
 		super(Slot5Button, self).__init__(*args, **kwargs)
 
-class Slot6Button(QPushButton):
+class Slot6Button(SlotButton):
 	def __init__(self, *args, **kwargs):
 		super(Slot6Button, self).__init__(*args, **kwargs)
 
-class Slot7Button(QPushButton):
+class Slot7Button(SlotButton):
 	def __init__(self, *args, **kwargs):
 		super(Slot7Button, self).__init__(*args, **kwargs)
 
-class Slot8Button(QPushButton):
+class Slot8Button(SlotButton):
 	def __init__(self, *args, **kwargs):
 		super(Slot8Button, self).__init__(*args, **kwargs)
